1fa2307a0c69d2095b5e600581d38ef795486b08
Gosith Jayathilake <gosithjayathilake@Gosiths-MacBook-Air.local>
2023-11-16 15:52:00 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.get('/api',(req,res)=>{
+    res.json({
+        "users": ["userOne","userTwo","userThree"]
+    })
+})
+
+app.listen(3000,()=>{
+    console.log("server is up and running")
+})
